b63608b59339eb3144cdef423d92f8b6206efb4e
pingidentity-sk <93611276+pingidentity-sk@users.noreply.github.com>
2022-01-27 11:58:57 +0100

    describe the object received by a capability
    Adds a lot of potentially useful information to the developer
    for what the capability receives when it fires

--- index.js
+++ index.js
@@ -32,8 +32,39 @@ const initialize = async () => {
  * This method performs the necessary processing for the
  * postHTTP capability for this example connector.
  * Note that the naming is important
- * @param {*} param0
+ * @param {*} inputPayload
  * @returns
+ *
+ * inputPayload /may/ contain the following properties:
+ *   => name: the name of the connector as set in the manifest 'name' top-level key
+ *         type: string
+ *   => companyId: the ID of the company the connector capability was called from
+ *         type: string
+ *   => flowId: the ID of the flow executing the connector capability
+ *         type: string
+ *   => flowVersionId: the version of the flow executing the connector capability
+ *         type: number
+ *   => connectionId: the ID of the connection (i.e. the instance of the connector with its own configuration settings)
+ *         type: string
+ *   => connectorId: the ID of the connector (i.e. the connector itself)
+ *         type: string
+ *   => capabilityName: the name of the capability being called. For this example it would be 'postHTTP'
+ *         type: string
+ *   => properties: the JSON object containing the properties passed into the capability, think of it as the values configured in the UI for the connection
+ *         type: object
+ *   => debugMode: whether debug mode is turned on. It is a good idea to leverage this to add increased level of logging in your connector code
+ *         type: boolean
+ *   => isDisabled: the connector is disabled. You may hadnle this gracefully by treating this as a hint to run the capability code as a dry run.
+ *         type: boolean
+ *   => respondToUser: 
+ *         type: boolean
+ *   => inputs: 
+ *         type: array
+ *   => interactionId: the identifier string for the interaction 
+ *         type: string
+ *   => connection: redundant information repeating top-level connectionId and connectorId
+ *         type: object
+ *
  */
 const handle_capability_postHTTP = async ({ properties }) => {
   logger.info('overriding handle_capability_postHTTP');
