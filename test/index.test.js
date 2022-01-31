058ef982404e945ba3a9d800480b285d1c4bdb79
samir-gandhi <samirgandhi@pingidentity.com>
2022-01-31 21:45:46 +0000

    better tests - not always pass

--- test/index.test.js
+++ test/index.test.js
@@ -23,28 +23,36 @@ describe('Unit Tests', () => {
       .stub(api, 'postHTTP')
       .returns({ data: { prop: 'value' } });
 
-    handle_capability_postHTTP(props.data)
-      .then((res) => {
+    handle_capability_postHTTP(props.data).then(
+      (res) => {
         console.log(`res is: ${JSON.stringify(res)}`);
         expect(res).to.be.an('object');
         expect(res.output).to.be.an('object');
         expect(res.output.rawResponse).to.be.an('object');
-        expect(res.output.rawResponse).should.have.property('prop');
-      })
-      .finally(() => {
+        expect(res.output.rawResponse).to.have.property('prop');
         stub1.restore();
         done();
-      });
+      },
+      (err) => {
+        console.log(`error is: ${error}`);
+        stub1.restore();
+        done();
+      }
+    );
   });
 
   it('handle missing url', (done) => {
-    handle_capability_postHTTP(props.badData)
-      .catch((error) => {
+    handle_capability_postHTTP(props.badData).then(
+      (response) => {
+        console.log(`response is ${response}`);
+        expect(response).to.not.exist;
+        done();
+      },
+      (error) => {
         console.log(`error is: ${JSON.stringify(error)}`);
         expect(error.code).to.be.a('string');
-      })
-      .finally(() => {
         done();
-      });
+      }
+    );
   });
 });
