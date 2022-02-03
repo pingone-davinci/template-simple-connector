621d78449fffc66c8ee947ef4112a1643965647b
Terry Sigle <terry@sigle.com>
2022-02-03 12:15:12 -0700

    Changing back to return, as dev's should be using the update-manifest script or simply ctrl-c

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const initialize = async () => {
     // Update Manifest
     if (get(process, 'argv[2]', null) === 'mode=update-manifest') {
       await sdk.manifestDeploy(connectorManifest);
-      process.exit(0);
+      return;
     }
     // The real thing of note here: registers the connector with the SDK and subscribes to REDIS changes
     const response = await sdk.initalize(redisList);
