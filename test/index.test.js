37b2e102c1f569f31dc7ab170158bd2c0e9c1d14
PingOne DaVinci <93611276+pingone-davinci@users.noreply.github.com>
2022-02-16 16:45:25 -0700

    Update index.test.js

--- test/index.test.js
+++ test/index.test.js
@@ -11,14 +11,9 @@ const api = require('../api');
 const { handle_capability_postHTTP } = require('../index');
 
 const props = require('./data/properties');
-const { assert } = require('sinon');
-
-before((done) => {
-  done();
-});
 
 describe('Unit Tests', () => {
-  it('happy path', (done) => {
+  it('happy path', async () => {
     const stub1 = sinon
       .stub(api, 'postHTTP')
       .returns({ data: { prop: 'value' } });
@@ -31,27 +26,23 @@ describe('Unit Tests', () => {
         expect(res.output.rawResponse).to.be.an('object');
         expect(res.output.rawResponse).to.have.property('prop');
         stub1.restore();
-        done();
       },
       (err) => {
         console.log(`error is: ${error}`);
         stub1.restore();
-        done();
       }
     );
   });
 
-  it('handle missing url', (done) => {
+  it('handle missing url', async () => {
     handle_capability_postHTTP(props.badData).then(
       (response) => {
         console.log(`response is ${response}`);
         expect(response).to.not.exist;
-        done();
       },
       (error) => {
         console.log(`error is: ${JSON.stringify(error)}`);
         expect(error.code).to.be.a('string');
-        done();
       }
     );
   });
