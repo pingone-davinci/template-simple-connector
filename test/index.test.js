3d471213931d63eafa5f190ba46393516e8382b2
Terry Sigle <terry@sigle.com>
2022-04-05 16:53:46 -0600

    GSA-86 - creating sdkInitilizeTest.js

--- test/index.test.js
+++ test/index.test.js
@@ -3,28 +3,31 @@
 /**
  * Singularkey Unit Tests
  */
-const { expect } = require('chai');
-const sinon = require('sinon');
-const { logger } = require('@skinternal/skconnectorsdk');
-const api = require('../api');
+const { expect } = require("chai");
+const sinon = require("sinon");
+const { logger } = require("@skinternal/skconnectorsdk");
+const api = require("../api");
+const { sdkInitializeTest } = require("./sdkInitializeTest.js");
 
-const { handle_capability_postHTTP } = require('../index');
+const { handle_capability_postHTTP } = require("../index");
 
-const props = require('./data/properties');
+const props = require("./data/properties");
 
-describe('Unit Tests', () => {
-  it('happy path', async () => {
+sdkInitializeTest();
+
+describe("Unit Tests", () => {
+  it("happy path", async () => {
     const stub1 = sinon
-      .stub(api, 'postHTTP')
-      .returns({ data: { prop: 'value' } });
+      .stub(api, "postHTTP")
+      .returns({ data: { prop: "value" } });
 
     handle_capability_postHTTP(props.data).then(
       (res) => {
         logger.info(`res is: ${JSON.stringify(res)}`);
-        expect(res).to.be.an('object');
-        expect(res.output).to.be.an('object');
-        expect(res.output.rawResponse).to.be.an('object');
-        expect(res.output.rawResponse).to.have.property('prop');
+        expect(res).to.be.an("object");
+        expect(res.output).to.be.an("object");
+        expect(res.output.rawResponse).to.be.an("object");
+        expect(res.output.rawResponse).to.have.property("prop");
         stub1.restore();
       },
       (err) => {
@@ -34,7 +37,7 @@ describe('Unit Tests', () => {
     );
   });
 
-  it('handle missing url', async () => {
+  it("handle missing url", async () => {
     handle_capability_postHTTP(props.badData).then(
       (response) => {
         console.log(`response is ${response}`);
@@ -42,7 +45,7 @@ describe('Unit Tests', () => {
       },
       (error) => {
         console.log(`error is: ${JSON.stringify(error)}`);
-        expect(error.code).to.be.a('string');
+        expect(error.code).to.be.a("string");
       }
     );
   });
---
+++ test/sdkInitializeTest.js
@@ -0,0 +1,35 @@
+/**
+ * DaVinci Connector SDK Initialize Tests
+ */
+const { expect, use } = require("chai");
+const chaiAsPromised = require("chai-as-promised");
+const sinon = require("sinon");
+const sdk = require("@skinternal/skconnectorsdk");
+const rewire = require("rewire");
+
+use(chaiAsPromised);
+
+const sdkInitializeTest = async () => {
+  describe("SDK Initialize Tests", () => {
+    it("initialize: check for error", async () => {
+      // stub.restore();
+      sdkStub = sinon.stub(sdk, "initalize").throws("error");
+      const init = rewire("../index");
+      const initialize = init.__get__("initialize");
+      expect(initialize()).to.eventually.be.fulfilled;
+      sdkStub.restore();
+    });
+    it("Update Manifest", async () => {
+      process.argv[2] = "mode=update-manifest";
+      sdkStub = sinon.stub(sdk, "manifestDeploy");
+      const init = rewire("../index");
+      const initialize = init.__get__("initialize");
+      actualResponse = await initialize();
+      sdkStub.restore();
+    });
+  });
+};
+
+module.exports = {
+  sdkInitializeTest,
+};
