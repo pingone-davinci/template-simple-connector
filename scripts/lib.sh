cb5540b3d5b7daf73930a19623a8727cf01768a2
samir-gandhi <samirgandhi@pingidentity.com>
2022-04-14 22:22:11 +0000

    updates

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -19,8 +19,10 @@ installDependencies(){
 
 lint(){
   npx eslint -c ./utils/scripts/.eslintrc.json .
+  test $? -ne 0 && echo "ERROR: Failed Linting, Continuing"
 }
 
 unitTest(){
   nyc --check-coverage --lines 80 mocha 'test/*.test.js' --exit true --timeout 1000000
+  test $? -ne 0 && echo "ERROR: Failed Unit Tests, Continuing"
 }
